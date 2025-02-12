2868d791ff23a6b7fb95b7d8b2e285980209136a
ARNAB DHARA <adhara@sparkcognition.com>
2025-02-12 08:42:26 -0600

    updated remove error message

--- main2.py
+++ main2.py
@@ -1,8 +1,6 @@
 #First lines of code
 from langchain.llms import OpenAI
 
-# SECURE THIS KEY !!
-api_key = "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"
 
 llm = OpenAI(openai_api_key=api_key)
 result = llm("Tell me a joke")
